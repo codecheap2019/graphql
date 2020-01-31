0ad10fb05bde8d8067dca3e53d5966eac669aad9
codecheap2019 <52380928+codecheap2019@users.noreply.github.com>
2020-01-31 10:14:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+function trigger() {
 var { graphql, buildSchema } = require('graphql');
+var root = "https://codecheap-sql.herokuapp.com/v1/graphql";
 
 var schema = buildSchema(`
   type Query {
@@ -11,3 +13,4 @@ var root = { hello: () => 'Hello world!' };
 graphql(schema, '{ hello }', root).then((response) => {
   alert(response);
 });
+} 
